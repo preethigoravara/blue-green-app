6dc7125f4e317b0998f3469cbd8800295aae55f1
nkamlesh <46484920+nkamlesh@users.noreply.github.com>
2026-05-16 18:31:11 +0530

    Change response color and version in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1 style="color:black">Black version - v2.0</h1>');
+  res.end('<h1 style="color:Green">Green version - v3.0</h1>');
 });
 server.listen(80, () => console.log('Server running on port 80'));
